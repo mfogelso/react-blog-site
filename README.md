db002265f609be759590e30d5040dcae0f8b9d99
mfogelso <mfogelso@asu.edu>
2023-01-29 09:04:24 -0500

    add MongoDB database to backend

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # react-blog-site
- personal project putting React and other tools to work
+
+Personal project putting React and other tools to work
+
+---Notes for future projects---
+
+--Backend--
+-Initialize as npm package-
+Run `npm init -y`
+
+-Express-
+Install express with `npm install -save express`
+Can test with Postman app
+Can also look on browser at localhost:8000/somepath
+Run `npm install nodemon --save-dev` to only save as a development dependency
+Package.json file -> "dev": "nodemon src/server.js" to scripts for shortcut
+Can now run and auto update server with `npm run dev` to start server to listen for requests
+
+-Folder Setup-
+src folder and server.js file
+import express from 'express';
+const app = express();
+
+-Allow for imports-
+Package.json file -> add "type":"module"
+
+-MongoDB-
+Add folder in backend file for database
+Run `mongod --dbpath ./database-folder/`
+Run `mongosh` to run in terminal
+Create databse with `use some-name-db`. It will switch to the database
+Call database with db (i.e. `db.collectionName.insertMany()`)
+
+--Connecting Front and Backend--
+Go to front end directory in terminal and run `npm install axios`
+This will handle requests from urls
--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,57 +1,58 @@
 //add "type":"module" to package.json file at the top to allow for imports etc
 import express from 'express';
-
-//install express with npm install -save express
-//can test with Postman app
-//can also look on browser at localhost:8000/somepath
-//run npm install nodemon --save-dev to only save as a development dependency
-// add "dev": "nodemon src/server.js" to scripts in package.json file for shortcut
-//can now run and auto update server with `npm run dev` to start server
-
-let articlesInfo = [{
-    name: 'learn-react',
-    upvotes: 0,
-    comments: [],
-}, 
-{
-    name: 'learn-node',
-    upvotes: 0,
-    comments: [],
-}, {
-    name: 'mongodb',
-    upvotes: 0,
-    comments: [],
-}]
+import { db, connectToDb } from './db.js';
 
 const app = express();
 //allows express to parse json
 app.use(express.json());
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+//Get article info
+app.get('/api/articles/:name/', async (req, res) => {
     const { name } = req.params;
-    const article = articlesInfo.find(a => a.name === name);
+
+    const article = await db.collection('articles').findOne({ name });
+    if (article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+})
+
+app.put('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+
+    await db.collection('articles').updateOne({ name }, { 
+        //increment upvotes by 1 for MongoDB
+        $inc: { upvotes: 1 },
+    });
+
+    const article = await db.collection('articles').findOne({ name });
     if( article) {
-        article.upvotes += 1;
         res.send(`The ${name} article has ${article.upvotes} upvotes!`);
     } else {
         res.send('That article doesn\'t exist.');
     }
 });
 
-app.post('/api/articles/:name/comments', (req, res) => {
+app.post('/api/articles/:name/comments', async (req, res) => {
     const { postedBy, text } = req.body;
     const { name } = req.params;
 
-    const article = articlesInfo.find(a => a.name === name);
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    });
+
+    const article = await db.collection('articles').findOne({ name });
     if( article) {
-        article.comments.push({ postedBy, text });
         res.send(article.comments);
     } else {
         res.send('That article doesn\'t exist.');
     }
-
 });
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+//Server won't start until it's connected to the database
+connectToDb(() => {
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
 });
