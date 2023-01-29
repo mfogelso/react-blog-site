779531aaf5ad1806919d581776873b7bd4a42f68
mfogelso <mfogelso@asu.edu>
2023-01-29 14:10:51 -0500

    add back end integration to front end

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Personal project putting React and other tools to work
 
 ---Notes for future projects---
 
+--Front End--
+Run `npx create-react-app app-name`
+Run `npm start` to start up react server on local host
+
 --Backend--
 -Initialize as npm package-
 Run `npm init -y`
@@ -13,7 +17,7 @@ Install express with `npm install -save express`
 Can test with Postman app
 Can also look on browser at localhost:8000/somepath
 Run `npm install nodemon --save-dev` to only save as a development dependency
-Package.json file -> "dev": "nodemon src/server.js" to scripts for shortcut
+Package.json backend file -> "dev": "nodemon src/server.js" to scripts for shortcut
 Can now run and auto update server with `npm run dev` to start server to listen for requests
 
 -Folder Setup-
@@ -22,7 +26,7 @@ import express from 'express';
 const app = express();
 
 -Allow for imports-
-Package.json file -> add "type":"module"
+Package.json backend file -> add "type":"module"
 
 -MongoDB-
 Add folder in backend file for database
@@ -34,3 +38,6 @@ Call database with db (i.e. `db.collectionName.insertMany()`)
 --Connecting Front and Backend--
 Go to front end directory in terminal and run `npm install axios`
 This will handle requests from urls
+Run both `npm run dev` and `npm run start`
+Package.json file frontend -> at the top, "proxy": "http://localhost:8000/"
+This will make the front and backend think they are running on the same origin and restart front end
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,10 +1,35 @@
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import articles from './article-content';
+import AddCommentForm from '../components/AddCommentForm';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = () => {
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
+
+    //adding empty array at the end will only run the useEffect when the component is mounted
+    useEffect(() => {
+        //need to create a wrapper async function inside because you can't pass async as first prop in a function
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        };
+
+        loadArticleInfo();
+
+    }, [articleId]);
+
     const article = articles.find(article => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
 
     if (!article) {
         return <NotFoundPage />
@@ -13,10 +38,18 @@ const ArticlePage = () => {
     return (
         <>
             <h1>{article.title}</h1>
+            <div className='upvotes-section'>
+                <button onClick={addUpvote}>Upvote</button>
+                <p>This articles has {articleInfo.upvotes} upvote(s)</p>
+            </div>
             {article.content.map((paragraph, i) => (
                 //only use index for key when the content shown is static
                 <p key={i}>{paragraph}</p>
             ))}
+            <AddCommentForm 
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            <CommentsList comments={articleInfo.comments} />
         </>
 
     );
